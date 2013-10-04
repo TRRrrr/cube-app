ef3400de5ad92630ef6d6bd5b03fbd1e2594bdeb
fuqcool <fuqcool@gmail.com>
2013-10-04 17:52:37 -0400

    chart demo

---
+++ src/js/directive/chart.js
@@ -0,0 +1,34 @@
+module.directive('uiChart', function () {
+  return {
+    restrict: 'EACM',
+    template: '<div></div>',
+    replace: true,
+    link: function (scope, elem, attrs) {
+      var renderChart = function () {
+        var data = scope.$eval(attrs.chartData);
+        elem.html('');
+        if (!angular.isArray(data)) {
+          return;
+        }
+
+        var opts = {};
+        if (!angular.isUndefined(attrs.chartOptions)) {
+          opts = scope.$eval(attrs.chartOptions);
+          if (!angular.isObject(opts)) {
+            throw 'Invalid ui.chart options attribute';
+          }
+        }
+
+        elem.jqplot(data, opts);
+      };
+
+      scope.$watch(attrs.chartData, function () {
+        renderChart();
+      }, true);
+
+      scope.$watch(attrs.chartOptions, function () {
+        renderChart();
+      });
+    }
+  };
+});
--- src/js/stats.js
+++ src/js/stats.js
@@ -1,3 +1,29 @@
 module.controller('StatsCtrl', ['$scope', function ($scope) {
-  $scope.a = 'abc';
+  var options = {
+    seriesDefaults: {
+      // Make this a pie chart.
+      // renderer: jQuery.jqplot.CanvasAxisLabelRenderer,
+      rendererOptions: {
+        // Put data labels on the pie slices.
+        // By default, labels show the percentage of the slice.
+        showDataLabels: true
+      }
+    },
+    legend: { show: true, location: 'e' }
+  };
+
+  $scope.chartData = [[3,7,9,1,4,6,8,2,5]];
+
+  $scope.chartOptions = options;
+
+  $scope.changeType = function (chartType) {
+    debugger;
+    if (chartType === 'bar') {
+      options.seriesDefaults.renderer = jQuery.jqplot.BarRenderer;
+    } else if (chartType === 'line') {
+      options.seriesDefaults.renderer = undefined;
+    }
+
+    $scope.chartOptions = angular.copy(options);
+  };
 }]);
