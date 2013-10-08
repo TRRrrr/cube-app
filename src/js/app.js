c272560945a498c36a42ce2352688c99d15fa8f7
fuqcool <fuqcool@gmail.com>
2013-10-08 17:35:14 -0400

    add advice panel linetips pointed to demo image

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,39 @@
-module.controller('AppCtrl', ['$scope', function ($scope) {
+module.controller('AppCtrl', ['$scope', '$window', function ($scope, $window) {
   $scope.a = 'this is cool!';
+  $scope.showTip = false;
 
-  $scope.onClickTip = function () {
-
+  $scope.advicePanel = {
+    title: 'Advice',
+    advice: 'workind hard!'
   };
+
+  var onClickTip = function (part) {
+    return function () {
+      $scope.advicePanel.title = 'Advice for ' + part;
+    };
+  };
+
+  $scope.linetips = [
+    {
+      titleText: 'wrist',
+      content: '30->40',
+      position: [300, 400],
+      direction: 'top-left',
+      titleAngle: 30,
+      titleLength: 100,
+      contentLength: 120,
+      onClick: onClickTip('wrist')
+    },
+    {
+      titleText: 'neck',
+      content: '70->80',
+      position: [550, 300],
+      direction: 'top-right',
+      titleAngle: 45,
+      titleLength: 100,
+      contentLength: 120,
+      onClick: onClickTip('neck')
+    }
+  ];
 
 }]);
--- src/js/directive/linetip.js
+++ src/js/directive/linetip.js
@@ -5,22 +5,25 @@ module.directive('linetip', function () {
     replace: true,
     transclude: false,
     scope: {
-      position: '@',
+      direction: '=?',
       titleLength: '=?',
       titleAngle: '=?',
       contentLength: '=?',
-      titleText: '@',
-      content: '@',
+      titleText: '=?',
+      content: '=?',
+      position: '=?',
       onClick: '=?'
     },
     link: function (scope, el, attrs) {
-      scope.position = scope.position || 'top-right';
+      scope.direction = scope.direction || 'top-right';
       scope.titleLength = scope.titleLength || 100;
       scope.contentLength = scope.contentLength || 100;
       scope.titleAngle = scope.titleAngle || 30;
       scope.titleText = scope.titleText || '';
       scope.content = scope.content || '';
+      scope.position = scope.position || [0, 0];
       scope.onClick = scope.onClick || angular.noop;
+
 
       var $title = el.find('.tip-title');
       var $content = el.find('.tip-content');
@@ -56,10 +59,12 @@ module.directive('linetip', function () {
       $title.width(scope.titleLength);
       $content.width(scope.contentLength);
 
-      var posInfo = calculatePos(scope.position, scope.titleAngle, scope.titleLength,
+      var posInfo = calculatePos(scope.direction, scope.titleAngle, scope.titleLength,
                                  $title.height(), scope.contentLength);
       $title.css({ '-webkit-transform': 'rotate(' + posInfo.angle + 'deg)' });
       $content.css({ '-webkit-transform': 'translate(' + posInfo.offsetX + 'px, ' + posInfo.offsetY + 'px)' });
+
+      el.css({ left: scope.position[0], top: scope.position[1] });
 
       el.find('span').hover(function () {
         el.addClass('highlight');
